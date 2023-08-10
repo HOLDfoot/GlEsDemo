50e431a97f251ee012117a82bbff8f9c48f7f988
ZhuYongle <iamzhulele@163.com>
2023-08-10 10:40:37 +0800

    Add triangle模块

--- exmOpenGL/build/intermediates/merged_assets/debug/out/vertex.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec4 aPosition;
-attribute vec2 aTextureCoord;
-uniform mat4 uProjectMatrix;
-varying vec2 vTextureCoord;
-
-void main(){
-	gl_Position = uProjectMatrix * aPosition;
-	vTextureCoord = aTextureCoord;
-}
